76f459b09ec062b53328a890bac441da47cbeffe
Evan Boswood <evan@boswood.net>
2020-09-20 17:17:48 -0500

    updated back-end API routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const { transcode } = require('buffer');
 // Dependencies
 const path = require('path');
 const router = require('express').Router();
+const apiRoutes = require('./bookAPIRoutes');
+
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
