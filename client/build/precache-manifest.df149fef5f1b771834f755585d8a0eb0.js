595f5097da1e63f7f1c31a18fb7ac8038a475417
Evan Boswood <evan@boswood.net>
2020-09-20 17:15:48 -0500

    initial React build

---
+++ client/build/precache-manifest.df149fef5f1b771834f755585d8a0eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cab103ce21839c1c9570645bb4a0ae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4e200b6f37ab5eb902b",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a4e200b6f37ab5eb902b",
+    "url": "/static/js/2.7f33b7e7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7f33b7e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "914ec58a56df4c2702af",
+    "url": "/static/js/main.e246767c.chunk.js"
+  },
+  {
+    "revision": "8dc0203a9e40d0f2c0ac",
+    "url": "/static/js/runtime-main.5a039d6f.js"
+  }
+]);
