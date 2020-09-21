65d0e6fc89634222322ca562cd0998499919319b
Evan Boswood <evan@boswood.net>
2020-09-20 19:11:58 -0500

    new build

---
+++ client/build/precache-manifest.81ab407bd9efb18a4fab5ad13328a4ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7008e448174bfe1944b17d34e27d46ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4e0ac3e63936b50c42c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e4e0ac3e63936b50c42c",
+    "url": "/static/js/2.f71a8d37.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f71a8d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214b548d981dcff54e17",
+    "url": "/static/js/main.27a7d54c.chunk.js"
+  },
+  {
+    "revision": "8dc0203a9e40d0f2c0ac",
+    "url": "/static/js/runtime-main.5a039d6f.js"
+  }
+]);
