90481da00bc9cc910a3c887fff7f359b4261bc51
Evan Boswood <evan@boswood.net>
2020-09-20 19:08:44 -0500

    fixed rendering

--- server.js
+++ server.js
@@ -10,15 +10,21 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // CORS Setup
-app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+// Static Serve
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+  }
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
